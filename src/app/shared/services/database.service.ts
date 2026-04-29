990676ea8c0f9da71a764f69d414ff43ae642e29
Banu Iliuta <banuiliuta@gmail.com>
2026-04-29 16:19:18 +0300

    feat: add DatabaseService with Dexie.js

---
+++ src/app/shared/services/database.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import Dexie, { Table } from 'dexie';
+import { Note, Notebook, Tag } from '../models/note.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DatabaseService extends Dexie {
+    public notes!: Table<Note, number>;
+    public notebooks!: Table<Notebook, number>;
+    public tags!: Table<Tag, number>;
+
+    constructor() {
+        super('ZenJournalDb'){
+            this.version(1).stores({
+                note: '++id, title, notebookId, isArchived, createdAt, updatedAt',
+                notebooks: '++id, name, createdAt',
+                tags: '++id, name   '
+            });
+        }
+    }
+}
