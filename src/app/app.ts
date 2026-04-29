8d6d39c20c2e980ab8edeee0d70089648cf319c3
Banu Iliuta <banuiliuta@gmail.com>
2026-04-29 18:45:24 +0300

    feat: add NoteListComponent and integrate into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NoteListComponent } from './features/notes/note-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NoteListComponent],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
-  protected readonly title = signal('zen-journal');
+
 }
---
+++ src/app/features/notes/note-list.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, OnInit, importProvidersFrom, inject } from "@angular/core";
+import { DatePipe } from "@angular/common";
+import { NotesService } from "../../shared/services/notes.service";
+import { Note } from '../../shared/models/note.model';
+
+@Component({
+    selector: 'app-note-list',
+    standalone: true,
+    imports: [DatePipe],
+    templateUrl: './note-list.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NoteListComponent implements OnInit {
+    protected readonly notesService = inject(NotesService);
+
+    public async ngOnInit(): Promise<void> {
+        await this.notesService.loadNotes();
+    }
+
+    public selectNote(note: Note): void {
+        this.notesService.selectNote(note);
+    }
+}
