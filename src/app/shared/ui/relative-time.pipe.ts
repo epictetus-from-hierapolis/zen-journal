d0df284be560fd89d0076c1549de1093ae7b8f6b
Iliuta Banu <banuiliuta@gmail.com>
2026-06-26 11:46:37 +0300

    fix(relative-time): fix broken pipe for zoneless change detection

--- src/app/shared/ui/relative-time.pipe.ts
+++ src/app/shared/ui/relative-time.pipe.ts
@@ -1,13 +1,46 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { ChangeDetectorRef, inject, OnDestroy, Pipe, PipeTransform } from "@angular/core";
+import { interval, Subscription } from "rxjs";
 
 @Pipe({
     name: "relativeTime",
     standalone: true,
     pure: false,
 })
-export class RelativeTimePipe implements PipeTransform {
-    public transform(date: Date): string {
+export class RelativeTimePipe implements PipeTransform, OnDestroy {
+    private readonly cdr = inject(ChangeDetectorRef);
+    private latestValue: string = "";
+    private subscription: Subscription | null = null;
+    private lastDate: Date | null = null;
 
+    public transform(date: Date): string {
+        if (!this.lastDate || this.lastDate?.getTime() !== date.getTime()) {
+            this.lastDate = date;
+            this.updateValue(date);
+            this.setupTimer(date);
+        }
+        return this.latestValue;
+    }
+
+    private updateValue(date: Date): void {
+        const nextValue = this.calculateRelativeTime(date);
+
+        if (nextValue !== this.latestValue) {
+            this.latestValue = nextValue;
+            this.cdr.markForCheck();
+        }
+    }
+
+    private setupTimer(date: Date): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+
+        this.subscription = interval(30000).subscribe(() => {
+            this.updateValue(date);
+        });
+    }
+
+    private calculateRelativeTime(date: Date): string {
         const diff = Math.abs(new Date().valueOf() - date.valueOf()) / 1000; //secunde
 
         if (diff < 60) {
@@ -25,6 +58,14 @@ export class RelativeTimePipe implements PipeTransform {
                 year: 'numeric'
             }).format(date);
         }
+    }
+
+    public ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+
+
     }
 }
 
